68a4c9c0ab1f39cbfc1a27f0a2c48ad26aa55826
DO7OO from 07/2021 on <de8msh@gmx.de>
2025-10-03 13:42:47 +0200

    cred.h

--- cred.h
+++ cred.h
@@ -1,1 +1,6 @@
 // ===== WLAN eintragen =====
+const char* WIFI_SSID = "name";
+const char* WIFI_PASS = "pw";
+const char* WIFI_PASSWORD = "pw";
+const char* ssid = "name";
+const char* password = "pw";
